c23646d0fd712f0a8675103a99febe8b2de4973d
CRSylar <cristianoromaldetti@icloud.com>
2021-05-01 14:56:25 +0200

    adding png's

--- Srcs/main.py
+++ Srcs/main.py
@@ -1,9 +1,18 @@
 import pygame
+import random
 
 pygame.init()
+
+_sfondo_ = pygame.image.load('immagini/sfondo.png')
+_bird_	= pygame.image.load('immagini/uccello.png')
+_ground_ = pygame.image.load('immagini/base.png')
+_gameover_ = pygame.image.load('immagini/gameover.png')
+_pipedwn_ = pygame.image.load('immagini/tubo.png')
+_pipeup_ = pygame.transform.flip(_pipedwn_, False, True)
+
+WIN	= pygame.display.set_mode((288, 512))
+pygame.display.set_caption("Flappy Bird!")
 loop = True
-WIN = pygame.display.set_mode ((500, 500))
-pygame.display.set_caption("Hello World!")
 while loop:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
